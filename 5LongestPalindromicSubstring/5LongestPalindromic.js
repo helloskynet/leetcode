ea2492de7e8bbb627fccf01ac41b10831f647b84
liu <836886548@qq.com>
2017-12-19 15:24:33 +0800

    5LongestPalindRomic 21%

--- 5LongestPalindromicSubstring/5LongestPalindromic.js
+++ 5LongestPalindromicSubstring/5LongestPalindromic.js
@@ -9,108 +9,100 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
-    console.time("liu");
-    var ti = s.length;
-    for (var i = ti; i > 0; i--) {
-        for (var j = 0; j <= ti - i; j++) {
-            var m = s.substr(j, i);
-            console.log(m);
-            var len = m.split("").reverse().join("");
-            if (len === m) {
-                console.timeEnd("liu");
-                return m;
-            }
-        }
-    }
-
-};
-var longestPalindrome2 = function (s) {
-    console.time("liu2");
-    for (var i = 0; i < s.length; i++) {
-        for (var j = 0; j <= s.length - i; j++) {
-            var m = s.substr(j, i);
-            console.log(m);
-            var len = m.split("").reverse().join("");
-            if (len === m) {
-                console.timeEnd("liu2");
-                return m;
-            }
-        }
-    }
-
-};
-var longestPalindrome3 = function (s) {
+function longestPalindrome(s) {
     var len = s.length;
     if (len < 2) {
         return s;
     }
-    var max = 0;
-    var flag = 0;//如果连着两个长度都没又找到  说明没有更长的了
-    var r = 0;
-    for (var i = 1; i <= len; i++) {
-        flag2 = 0;
+    var max = 0;  //保存最长的回文字符串
+    var flag = 0; //如果某个长度都没又找到  说明没有更长的了
+    var r = 0;    //用于记录字符串开始位置
+    //第一次搜索长度为奇数的最长的的回文字符串
+    for (var i = 1; i <= len; i = i + 2) {
+        flag = 0;
         for (var j = r; j <= len - i; j++) {
             var m = s.substr(j, i);
+            console.log(m.length);
             if (test(m)) {
+
                 max = m;
-                flag = 0;
-                flag2 = 1;
+                flag = 1;
                 if (j >= 1) {
                     j--;
                 }
-                i++;
                 r = j;
                 break;
             }
         }
-        if (!flag2) {
-            flag++;
-        }
-        if (flag > 3) {
+        if (!flag) {
+            //有一个奇数长度没搜到，，说明没有更长的 长度为 奇数的回文字符串了
             break;
         }
     }
-    var i = max.length + 1;
-    for (var j = 0; j <= len - i; j++) {
-        var m = s.substr(j, i);
-        if (test(m)) {
-            max = m;
+    //最长的奇数长度的回文字符串 加 1 开始查找 偶数长度的回文字符串
+    r = 0;
+    for (var i = max.length + 1; i <= len; i = i + 2) {
+        flag = 0;
+        for (var j = r; j <= len - i; j++) {
+            var m = s.substr(j, i);
+            if (test(m)) {
+                max = m;
+                flag = 1;
+                if (j >= 1) {
+                    j--;
+                }
+                r = j;
+                break;
+            }
+        }
+        if (!flag) {
             break;
         }
     }
-
-    return max.length;
-};
+    return max;
+}
 var test = function (s) {
     var len = s.split("").reverse().join("");
     return len === s;
 };
+
 console.time("liu");
 //console.log(longestPalindrome3("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
-console.log(longestPalindrome4("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearthhtraeehtmorfhsireptonllahselpoepehtrofelpoepehtybelpoepehtfotnemnrevogtahtdnamodeerffohtribwenaevahllahsdoGredsnunoitansihttahtniavnideidevahtonllahsdaedesehttahtevloserylhgiherehewtahtnoitovedfoerusaemllufptsalehtevagyehthcihwrofesuactahtotnoitoveddesaercniekatewdaedderonohesehtmorftahtsuerofebgniniamerksfadttaergehtotdetacidederehebotsurofrehtarsitIdecnavdaylbonosrafsuhtevaherehthguofohwyehthcihwkrowdehsinifnluehtoterehdetacidedebotrehtargnivilehtsurofsitIerehdidyehttahwtegrofrevennactituberehyasewtahwrebmemergnolroneltonelttilllifwsdadlrowehgTtcartedroddaotrewnoproopruoevobaraftidetarcesnocevaherehdelggurtsohwdaeddnagnivilnemlevarbehTdnuorgsihtwollahtonnacewetarcesnoctonnacewetacidedtonnacewesnesregralanituBsihtoddluohsewtahtreporpdnagnafrehtegotlasitIevilthgimnoitantahttahtsevilriehtevagerehohwesohtrofecalpgnitserlanifasadleiftahtfonoitropaetapcidedotemocevaheWrawtahztfodlmeifelttabtaerganotemqeraeWerudnegnolnacdetacidedosdnadeviecnocosnoitranynaronoitpantahtrehtehwgnitsetrawlivic"));
+  console.log(longestPalindrome2("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearthhtraeehtmorfhsireptonllahselpoepehtrofelpoepehtybelpoepehtfotnemnrevogtahtdnamodeerffohtribwenaevahllahsdoGredsnunoitansihttahtniavnideidevahtonllahsdaedesehttahtevloserylhgiherehewtahtnoitovedfoerusaemllufptsalehtevagyehthcihwrofesuactahtotnoitoveddesaercniekatewdaedderonohesehtmorftahtsuerofebgniniamerksfadttaergehtotdetacidederehebotsurofrehtarsitIdecnavdaylbonosrafsuhtevaherehthguofohwyehthcihwkrowdehsinifnluehtoterehdetacidedebotrehtargnivilehtsurofsitIerehdidyehttahwtegrofrevennactituberehyasewtahwrebmemergnolroneltonelttilllifwsdadlrowehgTtcartedroddaotrewnoproopruoevobaraftidetarcesnocevaherehdelggurtsohwdaeddnagnivilnemlevarbehTdnuorgsihtwollahtonnacewetarcesnoctonnacewetacidedtonnacewesnesregralanituBsihtoddluohsewtahtreporpdnagnafrehtegotlasitIevilthgimnoitantahttahtsevilriehtevagerehohwesohtrofecalpgnitserlanifasadleiftahtfonoitropaetapcidedotemocevaheWrawtahztfodlmeifelttabtaerganotemqeraeWerudnegnolnacdetacidedosdnadeviecnocosnoitranynaronoitpantahtrehtehwgnitsetrawlivic"));
 //console.log(longestPalindrome4("babaddtattarrattatddetartrateedredividerb"));
-//console.log(longestPalindrome4("cbbd"));
-//console.log(longestPalindrome3("fdgdfg"));
+console.log(longestPalindrome2("ccc"));
 console.timeEnd("liu");
 
-
-function longestPalindrome4(s) {
+//步骤分解
+function longestPalindrome2(s) {
     var len = s.length;
     if (len < 2) {
         return s;
     }
-    var max = 0;
-    var flag = 0;//如果连着两个长度都没又找到  说明没有更长的了
+    var max = 0;  //保存最长的回文字符串
+    var flag = 0; //如果某个长度都没又找到  说明没有更长的了
+    var r = 0;    //用于记录字符串开始位置
+    //第一次搜索长度为奇数的最长的的回文字符串
+    var m1  = huiwen(1,s);
+    //最长的 奇数长度的回文字符串 加 1 开始查找 偶数长度的回文字符串
+   var m2 = huiwen(m1.length + 1 , s);
+   if(m1.length > m2.length){
+       return m1.length;
+   }else{
+       return m2.length;
+   }
+}
+function huiwen(start,s) {
+    var len = s.length;
     var r = 0;
-    for (var i = 1; i <= len; i = i + 2) {
-        flag2 = 0;
+    var max = "";
+    for (var i = start; i <= len; i = i + 2) {
+        flag = 0;
         for (var j = r; j <= len - i; j++) {
             var m = s.substr(j, i);
             if (test(m)) {
                 max = m;
-                flag = 0;
-                flag2 = 1;
+                flag = 1;
                 if (j >= 1) {
                     j--;
                 }
@@ -118,35 +110,9 @@ function longestPalindrome4(s) {
                 break;
             }
         }
-        if (!flag2) {
-            flag++;
-        }
-        if (flag > 3) {
+        if (!flag) {
             break;
         }
     }
-    r = 0;
-    for (var i = max.length+1; i <= len; i = i + 2) {
-        flag2 = 0;
-        for (var j = r; j <= len - i; j++) {
-            var m = s.substr(j, i);
-            if (test(m)) {
-                max = m;
-                flag = 0;
-                flag2 = 1;
-                if (j >= 1) {
-                    j--;
-                }
-                r = j;
-                break;
-            }
-        }
-        if (!flag2) {
-            flag++;
-        }
-        if (flag > 3) {
-            break;
-        }
-    }
-    return max.length;
+    return max;
 }
